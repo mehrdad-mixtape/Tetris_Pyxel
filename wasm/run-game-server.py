6ce6aac1214396a2a696cf7d22450e353504982d
mehrdad-mixtape <mehrdad0011998@gmail.com>
2023-10-13 00:53:41 +0330

    New Changes: Discription 1. help of run-game-server.py updated after long time :)

--- wasm/run-game-server.py
+++ wasm/run-game-server.py
@@ -8,7 +8,7 @@ class MyHttpRequestHandler(http.server.SimpleHTTPRequestHandler):
 def main() -> None:
     Handler = MyHttpRequestHandler
     parser = argparse.ArgumentParser()
-    parser.add_argument('-a', '--addr', help="bind address to server (default is 127.0.0.1)", required=False)
+    parser.add_argument('-a', '--addr', help="bind address to server (default is 127.0.0.1)", type=str, required=False)
     parser.add_argument('-p', '--port', help="bind port to address of server (default is 8000)", type=int, required=False)
     args = parser.parse_args()
 
@@ -24,12 +24,11 @@ def main() -> None:
         print(f"""
             PyxelTetris Server Running on {ADDR}:{PORT}
             1. Open Your Browser And Go to URL: http://{ADDR}:{PORT}
-            2. Click On 'game.html'
-            3. Wait For Minutes (depends on your internet-quality)
-            Do You Wanna Help? Run This:
-                $ run-game-server-linux --help
-                $ run-game-server-windows.exe --help
-                $ python3 server.py --help
+            2. Wait For Minutes (depends on your internet-quality) And Game Will Be Start.
+            Do You Wanna Change {ADDR=} | {PORT=} ?
+                $ python3 run_game_server.py --addr ADDR --port PORT
+                $ python3 run_game_server.py --addr ADDR
+                $ python3 run_game_server.py --port PORT
             Press Ctrl+C to Exit ..."""
         )
         httpd.serve_forever()
